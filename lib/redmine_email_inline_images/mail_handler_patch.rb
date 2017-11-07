3d0de0c8ebef4c16bed2f8c0731b30d81bc267f1
Andriy Lesyuk <s-andy@andriylesyuk.com>
2017-11-08 00:30:59 +0200

    [FIX] Use HTML part, check all parts for images + better HTML-to-text
    - Use plain text part, only if HTML part is unavailable
    - Look up inline images in nested parts
    - Use Redmine's html_body_to_text to strip HTML tags
    - Remove white spaces before contents

--- lib/redmine_email_inline_images/mail_handler_patch.rb
+++ lib/redmine_email_inline_images/mail_handler_patch.rb
@@ -15,11 +15,11 @@ module RedmineEmailInlineImages
       # an issue created by an email request
       def plain_text_body_with_email_inline_images
         return @plain_text_body unless @plain_text_body.nil?
-        part = email.text_part || email.html_part || email
+        part = email.html_part || email.text_part || email
         @plain_text_body = Redmine::CodesetUtil.to_utf8(part.body.decoded, part.charset)
     
         email_images = {}
-        email.part.each do |part|
+        email.all_parts.each do |part|
             if part['Content-ID']
                 cid = "cid:#{part['Content-ID'].element.message_ids[0]}"
                 image = part.header['Content-Type'].parameters['name']
@@ -30,14 +30,21 @@ module RedmineEmailInlineImages
         # replace html images with text bang notation
         email_doc = Nokogiri::HTML(@plain_text_body)
         email_doc.xpath('//img').each do |image|
-            image_bang = "\n!#{email_images[image['src']]}!"
-            image.replace(email_doc.create_text_node(image_bang))
+            case Setting.text_formatting
+            when 'markdown'
+                image_bang = "\n![](#{email_images[image['src']]})"
+            when 'textile'
+                image_bang = "\n!#{email_images[image['src']]}!"
+            else
+                image_bang = nil
+            end
+            image.replace(email_doc.create_text_node(image_bang)) if image_bang
         end
         @plain_text_body = email_doc.to_html
         
         # strip html tags and remove doctype directive
-        @plain_text_body = strip_tags(@plain_text_body.strip)
-        @plain_text_body.sub! %r{^<!DOCTYPE .*$}, ''
+        @plain_text_body = self.class.html_body_to_text(@plain_text_body)
+        @plain_text_body.gsub! %r{^[\t ]+}, ''
         @plain_text_body
       end
       
