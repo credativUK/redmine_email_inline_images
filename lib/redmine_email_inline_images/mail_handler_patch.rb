49eed9419d0b5b7a0ca6448ee46f33ce8b63edba
Drako Su <drakosu@aaeon.com.tw>
2023-02-20 16:17:42 +0800

    Support inline image which having same file name in email raw data
    Reason: Otherwise last picture will always be displayed. This happens on email from China

--- lib/redmine_email_inline_images/mail_handler_patch.rb
+++ lib/redmine_email_inline_images/mail_handler_patch.rb
@@ -5,6 +5,7 @@ module RedmineEmailInlineImages
       
       base.class_eval do
         alias_method_chain :plain_text_body, :email_inline_images
+        alias_method_chain :add_attachments, :cid_name
         alias_method_chain :accept_attachment?, :checking_truncation
         alias_method_chain :issue_attributes_from_keywords, :parent_id
       end
@@ -25,13 +26,7 @@ module RedmineEmailInlineImages
         email_images = {}
         email.all_parts.each do |part|
             if part['Content-ID']
-                if part['Content-ID'].respond_to?(:element)
-                    content_id = part['Content-ID'].element.message_ids[0]
-                else
-                    content_id = part['Content-ID'].value.gsub(%r{(^<|>$)}, '')
-                end
-                image = part.header['Content-Type'].parameters['name']
-                email_images["cid:#{content_id}"] = image
+                email_images["cid:#{cid(part)}"] = short_cid_name(part)
             end
         end
         
@@ -62,6 +57,26 @@ module RedmineEmailInlineImages
         @plain_text_body_with_inline_images
       end
 
+      def cid(part)
+        if part['Content-ID'].respond_to?(:element)
+          content_id = part['Content-ID'].element.message_ids[0]
+        else
+          content_id = part['Content-ID'].value.gsub(%r{(^<|>$)}, '')
+        end
+        content_id
+      end
+
+      def short_cid_name(attachment)
+        if attachment['Content-ID']
+          content_id = cid(attachment)
+          cid_name = content_id[-8,8] + '-' + attachment.header['Content-Type'].parameters['name']
+        else
+          # fail-safe to filename
+          cid_name = attachment.filename.to_s
+        end
+        cid_name
+      end
+
       # Returns false if the +attachment+'s name of the incoming email should be ignored
       def accept_attachment_name?(attachment_name)
         @excluded ||= Setting.mail_handler_excluded_filenames.to_s.split(',').map(&:strip).reject(&:blank?)
@@ -74,13 +89,27 @@ module RedmineEmailInlineImages
         end
         true
       end
-      
+
+      def add_attachments_with_cid_name(obj)
+        if email.attachments && email.attachments.any?
+          email.attachments.each do |attachment|
+            next unless accept_attachment?(attachment)
+            obj.attachments << Attachment.create(:container => obj,
+                              :file => attachment.body.decoded,
+                              :filename => short_cid_name(attachment),
+                              :author => user,
+                              :content_type => attachment.mime_type)
+          end
+        end
+      end
+    
       # Returns false if the +attachment+ is a truncated inline image, or the +attachment+ of the incoming email should be ignored by name.
       def accept_attachment_with_checking_truncation?(attachment)
+        attachment_name = short_cid_name(attachment)
         @truncated_inline_images ||= find_inline_images_from_body(truncated_plain_text_body)
         @truncated_inline_images.each do |filename|
-          if attachment.filename.to_s == filename
-            logger.info "MailHandler: ignoring attachment #{attachment.filename} matching truncated inline image #{filename}"
+          if attachment_name == filename
+            logger.info "MailHandler: ignoring attachment #{attachment_name} matching truncated inline image #{filename}"
             return false
           end
         end unless @truncated_inline_images.nil?
